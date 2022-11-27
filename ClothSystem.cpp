221030b8d81c2ad8258ed9b885c5b8630d32a645
Jess <35327895+JessStandardVer@users.noreply.github.com>
2022-11-27 20:38:06 +0800

    Update ClothSystem.cpp

--- ClothSystem.cpp
+++ ClothSystem.cpp
@@ -1,168 +1,210 @@
 #include "ClothSystem.h"
+#include <cmath>
 
-ClothSystem::ClothSystem(int width, int height, int systemState)
+ClothSystem::ClothSystem(int width, int height, int systemState[])
 {
-    this->width = width;
-    this->height = height;
-    this->systemState = systemState;
-    this->m_numParticles = width * height;
-    this->fixed_points = vector<bool>(width);
-    //fix points
-    fixed_points[0] = 1;
-    fixed_points[width-1] = 1;
-    int step = width/4;
-    for(int i = step;i<width-1;i +=step){
-        fixed_points[i] = 1;
-    }
-    //no motion
-    Vector3f velocity(0,0,0);
-    Vector3f originPos(-3,5,-13.1);
-    for(int i = 0;i<height;i++){
-        for(int j = 0;j<width;j++){
-            Vector3f position(j*particleDistance,-i*particleDistance,0);
-            position += originPos;
-            //initialize state
-            m_vVecState.push_back(position);
-            m_vVecState.push_back(velocity);
-        }
-    }
+	this->width = width;
+	this->height = height;
+	this->systemState = systemState[0];
+	this->m_numParticles = width * height;
+	this->fixed_points = vector<bool>(width);
+	//fix points
+	fixed_points[0] = 1;
+	fixed_points[width - 1] = 1;
+	int step = width / 4;
+	for (int i = step; i < width - 1; i += step) {
+		fixed_points[i] = 1;
+	}
+	//no motion
+	Vector3f velocity(0, 0, 0);
+	Vector3f originPos(-3, 5, -13.1);
+	originPos.x() += systemState[1] * 0.2f;
+	originPos.y() += systemState[2] * 0.2f;
+	originPos.z() += systemState[3] * 0.2f;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			Vector3f position(j*particleDistance, -i * particleDistance, 0);
+			position += originPos;
+			//initialize state
+			m_vVecState.push_back(position);
+			m_vVecState.push_back(velocity);
+		}
+	}
 }
 
 //helper function
 // given index i,j into a n x n cloth, returns the linear index into vector of particles
 int ClothSystem::indexOf(int i, int j) {
-    return i * width + j;
+	return i * width + j;
 }
 
 void ClothSystem::move() {
-    for(int j = 0;j<fixed_points.size();j++){
-        bool fixed = fixed_points[j];
-        if(fixed){
-            int pin = indexOf(0,j);
-            float z = m_vVecState[2*pin].z();
-            //limit the moving range
-            if(z>moveUpperBound){
-                moveDir = Vector3f(0,0,-1);
-            }else if(z<moveLowerBound){
-                moveDir = Vector3f(0,0,1);
-            }
-            m_vVecState[2 * pin] += moveDir*Vector3f(0,0,0.06);
-        }
-    }
+	for (int j = 0; j < fixed_points.size(); j++) {
+		bool fixed = fixed_points[j];
+		if (fixed) {
+			int pin = indexOf(0, j);
+			float z = m_vVecState[2 * pin].z();
+			//limit the moving range
+			if (z > moveUpperBound) {
+				moveDir = Vector3f(0, 0, -1);
+			}
+			else if (z < moveLowerBound) {
+				moveDir = Vector3f(0, 0, 1);
+			}
+			m_vVecState[2 * pin] += moveDir * Vector3f(0, 0, 0.06);
+		}
+	}
+}
+
+//helper function
+// compare between f and frictionForce
+float ClothSystem::compareForce(float f, float friction) {
+	if(abs(f) <= abs(friction)){
+		return 0;
+	}
+	else {
+		if ((f < 0 && friction < 0) || (f > 0 && friction > 0)) {
+			return f - friction;
+		}
+		else {
+			return f + friction;
+		}
+	}
 }
 
 // for a given state, evaluate f(X,t)
 vector<Vector3f> ClothSystem::evalF(vector<Vector3f> state)
 {
-    vector<Vector3f> f;
-    //gravity force is constant->m*g
-    Vector3f fGravity = mass * gravity;
-    for(int i = 0;i<height;i++){
-        for(int j = 0;j<width;j++){
-            int curIndex = indexOf(i,j);
-            //fix n particles - hang the cloth
-            if (systemState != 1) {
-                if(i == 0 && fixed_points[j]){
-                    f.push_back(Vector3f(0.0f));
-                    f.push_back(Vector3f(0.0f));
-                    continue;
-                }
-            }
-            Vector3f position = state[2*curIndex];
-            Vector3f velocity = state[2*curIndex+1];
-            //drag force
-            Vector3f fDrag = -1 * dampling * velocity;
-            //structural spring force
-            Vector3f fStructuralSpring;
-            //means exist a neighbor on the top
-            bool top = i>0;
-            bool bottom = i<height-1;
-            bool left = j>0;
-            bool right = j<width-1;
-            //top
-            if(top){
-                int newIndex = indexOf(i-1,j);
-                Vector3f distance = position - state[2*newIndex];
-                fStructuralSpring += structuralStiffness*(structuralRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //bottom
-            if(bottom){
-                int newIndex = indexOf(i+1,j);
-                Vector3f distance = position - state[2*newIndex];
-                fStructuralSpring += structuralStiffness*(structuralRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //left
-            if(left){
-                int newIndex = indexOf(i,j-1);
-                Vector3f distance = position - state[2*newIndex];
-                fStructuralSpring += structuralStiffness*(structuralRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //right
-            if(right){
-                int newIndex = indexOf(i,j+1);
-                Vector3f distance = position - state[2*newIndex];
-                fStructuralSpring += structuralStiffness*(structuralRestLength-distance.abs()) * (distance/distance.abs());
-            }
+	vector<Vector3f> f;
+	//gravity force is constant->m*g
+	Vector3f fGravity = mass * gravity;
+	Vector3f ball = Vector3f(0, -0.25f * height, 0);
+	float epsilon = 0.125f;
+	float rad = 0.5f * particleDistance* width;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			int curIndex = indexOf(i, j);
+			//fix 2 particles - hang the cloth
+			if (systemState == 0) {
+				if ((i == 0 && j == 0) || (i == 0 && j == width - 1)) {
+					f.push_back(Vector3f(0.0f));
+					f.push_back(Vector3f(0.0f));
+					continue;
+				}
+			}
+			
+			Vector3f position = state[2 * curIndex];
+			Vector3f velocity = state[2 * curIndex + 1];
+			//drag force
+			Vector3f fDrag = -1 * dampling * velocity;
+			//structural spring force
+			Vector3f fStructuralSpring;
+			//means exist a neighbor on the top
+			bool top = i > 0;
+			bool bottom = i < height - 1;
+			bool left = j > 0;
+			bool right = j < width - 1;
+			//top
+			if (top) {
+				int newIndex = indexOf(i - 1, j);
+				Vector3f distance = position - state[2 * newIndex];
+				fStructuralSpring += structuralStiffness * (structuralRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//bottom
+			if (bottom) {
+				int newIndex = indexOf(i + 1, j);
+				Vector3f distance = position - state[2 * newIndex];
+				fStructuralSpring += structuralStiffness * (structuralRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//left
+			if (left) {
+				int newIndex = indexOf(i, j - 1);
+				Vector3f distance = position - state[2 * newIndex];
+				fStructuralSpring += structuralStiffness * (structuralRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//right
+			if (right) {
+				int newIndex = indexOf(i, j + 1);
+				Vector3f distance = position - state[2 * newIndex];
+				fStructuralSpring += structuralStiffness * (structuralRestLength - distance.abs()) * (distance / distance.abs());
+			}
 
-            //shear spring force
-            Vector3f fShearSpring;
-            //left top
-            if(left & top){
-                int newIndex = indexOf(i-1,j-1);
-                Vector3f distance = position - state[2*newIndex];
-                fShearSpring += shearStiffness*(shearRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //left bottom
-            if(left & bottom){
-                int newIndex = indexOf(i+1,j-1);
-                Vector3f distance = position - state[2*newIndex];
-                fShearSpring += shearStiffness*(shearRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //right top
-            if(right & top){
-                int newIndex = indexOf(i-1,j+1);
-                Vector3f distance = position - state[2*newIndex];
-                fShearSpring += shearStiffness*(shearRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //right bottom
-            if(right & bottom){
-                int newIndex = indexOf(i+1,j+1);
-                Vector3f distance = position - state[2*newIndex];
-                fShearSpring += shearStiffness*(shearRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //flex spring force
-            Vector3f fFlexSpring;
-            //top
-            if(i>1){
-                int newIndex = indexOf(i-2,j);
-                Vector3f distance = position - state[2*newIndex];
-                fFlexSpring += flexStiffness*(flexRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //bottom
-            if(i<height-2){
-                int newIndex = indexOf(i+2,j);
-                Vector3f distance = position - state[2*newIndex];
-                fFlexSpring += flexStiffness*(flexRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //left
-            if(j>1){
-                int newIndex = indexOf(i,j-2);
-                Vector3f distance = position - state[2*newIndex];
-                fFlexSpring += flexStiffness*(flexRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //right
-            if(j<width-2){
-                int newIndex = indexOf(i,j+2);
-                Vector3f distance = position - state[2*newIndex];
-                fFlexSpring += flexStiffness*(flexRestLength-distance.abs()) * (distance/distance.abs());
-            }
-            //net force
-            Vector3f fNet = fGravity+fDrag+fStructuralSpring+fShearSpring+fFlexSpring;
-            Vector3f acceleration = fNet/mass;
-            f.push_back(velocity);
-            f.push_back(acceleration);
-        }
-    }
+			//shear spring force
+			Vector3f fShearSpring;
+			//left top
+			if (left & top) {
+				int newIndex = indexOf(i - 1, j - 1);
+				Vector3f distance = position - state[2 * newIndex];
+				fShearSpring += shearStiffness * (shearRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//left bottom
+			if (left & bottom) {
+				int newIndex = indexOf(i + 1, j - 1);
+				Vector3f distance = position - state[2 * newIndex];
+				fShearSpring += shearStiffness * (shearRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//right top
+			if (right & top) {
+				int newIndex = indexOf(i - 1, j + 1);
+				Vector3f distance = position - state[2 * newIndex];
+				fShearSpring += shearStiffness * (shearRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//right bottom
+			if (right & bottom) {
+				int newIndex = indexOf(i + 1, j + 1);
+				Vector3f distance = position - state[2 * newIndex];
+				fShearSpring += shearStiffness * (shearRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//flex spring force
+			Vector3f fFlexSpring;
+			//top
+			if (i > 1) {
+				int newIndex = indexOf(i - 2, j);
+				Vector3f distance = position - state[2 * newIndex];
+				fFlexSpring += flexStiffness * (flexRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//bottom
+			if (i < height - 2) {
+				int newIndex = indexOf(i + 2, j);
+				Vector3f distance = position - state[2 * newIndex];
+				fFlexSpring += flexStiffness * (flexRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//left
+			if (j > 1) {
+				int newIndex = indexOf(i, j - 2);
+				Vector3f distance = position - state[2 * newIndex];
+				fFlexSpring += flexStiffness * (flexRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			//right
+			if (j < width - 2) {
+				int newIndex = indexOf(i, j + 2);
+				Vector3f distance = position - state[2 * newIndex];
+				fFlexSpring += flexStiffness * (flexRestLength - distance.abs()) * (distance / distance.abs());
+			}
+			
+			//friction force
+			float frictionForce;
+			int newIndex = indexOf(i, j);
+			if (systemState == 2) {
+				if ((m_vVecState[2 * newIndex] - ball).abs() <= (rad + epsilon)) {
+					frictionForce = frictionRate;
+				}else {
+					frictionForce = 0;
+				}
+			}		
+			
+			//net force
+			Vector3f fNet = fGravity + fDrag + fStructuralSpring + fShearSpring + fFlexSpring;
+			if (systemState == 2) {
+				fNet.x() = compareForce(fNet.x(), frictionForce);
+				fNet.y() = compareForce(fNet.y(), frictionForce);
+				fNet.z() = compareForce(fNet.z(), frictionForce);
+			}
+			Vector3f acceleration = fNet / mass;
+			f.push_back(velocity);
+			f.push_back(acceleration);
+		}
+	}
 	return f;
 
 
@@ -175,7 +217,7 @@ void ClothSystem::draw()
 	glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
 	glEnable(GL_COLOR_MATERIAL);
 	glColor3f(1.0f, 1.0f, 0.0f);
-	Vector3f ball = Vector3f(0.125f * width, -0.25f * height, -0.1f);
+	Vector3f ball = Vector3f(0, -0.25f * height, 0);
 	float rad = 0.5f * particleDistance* width;
 	float epsilon = 0.125f;
 	glPushMatrix();
@@ -188,29 +230,30 @@ void ClothSystem::draw()
 			m_vVecState[2 * n] = (ball + (rad + epsilon) * (m_vVecState[2 * n] - ball).normalized());
 		}
 	}
-	
-    for(int i = 0;i<height;i++){
-        for(int j = 0;j<width;j++){
-            //draw particle
-            Vector3f pos(m_vVecState[2* indexOf(i,j)]);
-            //draw structural springs
-            glLineWidth(2);
-            glBegin(GL_LINES);
-		float groundHeight = -0.25f * height;
-            //not in the last col
-            if(j<width-1){
-                Vector3f rightPos(m_vVecState[2* indexOf(i,j+1)]);
-                glVertex3f(pos[0], pos[1]< groundHeight ? groundHeight : pos[1], pos[2]);
-		glVertex3f(rightPos[0], rightPos[1] < groundHeight ? groundHeight : rightPos[1], rightPos[2]);
-            }
-            //not in the last row
-            if(i<height-1){
-                Vector3f bottomPos(m_vVecState[2* indexOf(i+1,j)]);
-                glVertex3f(pos[0], pos[1] < groundHeight ? groundHeight : pos[1], pos[2]);
-		glVertex3f(bottomPos[0], bottomPos[1] < groundHeight ? groundHeight : bottomPos[1], bottomPos[2]);
-            }
-            glEnd();
-        }
-    }
-}
 
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			//draw particle
+			Vector3f pos(m_vVecState[2 * indexOf(i, j)]);
+			//draw structural springs
+			glLineWidth(2);
+			glBegin(GL_LINES);
+			float groundHeight = -0.5f * height;
+			//not in the last col
+			if (j < width - 1) {
+				Vector3f rightPos(m_vVecState[2 * indexOf(i, j + 1)]);
+				glVertex3f(pos[0], pos[1]< groundHeight ? groundHeight : pos[1], pos[2]);
+				glVertex3f(rightPos[0], rightPos[1] < groundHeight ? groundHeight : rightPos[1], rightPos[2]);
+			}
+			//not in the last row
+			if (i < height - 1) {
+				Vector3f bottomPos(m_vVecState[2 * indexOf(i + 1, j)]);
+				glVertex3f(pos[0], pos[1] < groundHeight ? groundHeight : pos[1], pos[2]);
+				glVertex3f(bottomPos[0], bottomPos[1] < groundHeight ? groundHeight : bottomPos[1], bottomPos[2]);
+			}
+			glEnd();
+		}
+	}
+
+
+}
